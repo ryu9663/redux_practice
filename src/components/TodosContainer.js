5c58166d923ec628c14435e2287223f4b80c2795
june <ryu9663@naver.com>
2021-10-17 17:26:03 +0900

    added comment

--- src/components/TodosContainer.js
+++ src/components/TodosContainer.js
@@ -10,7 +10,11 @@ function TodosContainer() {
 
     const onCreate = text => dispatch(addTodo(text));
     // 최적화를 위해 useCallback 사용
+    //두번째 인자는 배열인데, useEffect처럼 배열안의 요소가 수정될때마다 첫번째 인자 콜백이 실행된다.
+    //배열값이 바뀌지 않으면 리렌더링하지 않고 이전값을 그대로 렌더링한다.
+    //렌더링 낭비를 줄인다.
     const onToggle = useCallback(id => dispatch(toggleTodo(id)), [dispatch])
+    // const onToggle = id => dispatch(toggleTodo(id));
 
     return <Todos todos = {todos} onCreate = {onCreate} onToggle={onToggle} />
 
